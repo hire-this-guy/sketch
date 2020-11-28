3b461c5e5f556f5820cdb56f74bbd7b0b7afc583
Krzyszof Lityński <klitynski@gog.com>
2020-11-28 20:20:26 +0100

    Thumbnails styling

--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -3,16 +3,19 @@ import React from "react";
 import "./Thumbnail.css";
 import { Link, useRouteMatch } from "react-router-dom";
 
-function Thumbnail(props: { artboard: Artboard; key: number }) {
+function Thumbnail(props: { artboard: Artboard }) {
     let { url } = useRouteMatch();
     return (
-        <Link to={`${url}/${props.artboard.name}`}>
-            <figure key={props.key}>
+        <Link to={`${url}/${props.artboard.name}`} className="thumbnail">
+            <figure className="thumbnail__figure">
                 <img
                     srcSet={`${props.artboard.files[0].url} 1x, ${props.artboard.files[1].url} 2x`}
-                    alt={props.artboard.name}
+                    alt=""
+                    className="thumbnail__img"
                 />
-                <figcaption>{props.artboard.name}</figcaption>
+                <figcaption className="thumbnail__caption">
+                    {props.artboard.name}
+                </figcaption>
             </figure>
         </Link>
     );
