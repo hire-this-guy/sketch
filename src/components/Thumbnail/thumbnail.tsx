dd3ab23bdbe2deec1f8887ca1fc8336a549e7f64
Krzyszof Lityński <klitynski@gog.com>
2020-11-28 11:40:15 +0100

    Thumbnails linking to artboard view

--- src/components/Thumbnail/thumbnail.tsx
+++ src/components/Thumbnail/thumbnail.tsx
@@ -1,16 +1,20 @@
 import { Artboard } from "../../types/response";
 import React from "react";
 import "./thumbnail.css";
+import { Link, useRouteMatch } from "react-router-dom";
 
 function Thumbnail(props: { artboard: Artboard; key: number }) {
+    let { url } = useRouteMatch();
     return (
-        <figure key={props.key}>
-            <img
-                srcSet={`${props.artboard.files[0].url} 1x, ${props.artboard.files[1].url} 2x`}
-                alt={props.artboard.name}
-            />
-            <figcaption>{props.artboard.name}</figcaption>
-        </figure>
+        <Link to={`${url}/${props.artboard.name}`}>
+            <figure key={props.key}>
+                <img
+                    srcSet={`${props.artboard.files[0].url} 1x, ${props.artboard.files[1].url} 2x`}
+                    alt={props.artboard.name}
+                />
+                <figcaption>{props.artboard.name}</figcaption>
+            </figure>
+        </Link>
     );
 }
 
