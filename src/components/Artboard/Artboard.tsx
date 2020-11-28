db2850e35beaa5128d8aad6ae1c30c82556267b3
Krzyszof Lityński <klitynski@gog.com>
2020-11-28 18:17:30 +0100

    Artboards navigation

--- src/components/Artboard/Artboard.tsx
+++ src/components/Artboard/Artboard.tsx
@@ -1,5 +1,5 @@
-import { Artboard, DocumentResponse } from "../../types/response";
-import { useParams } from "react-router-dom";
+import { DocumentResponse } from "../../types/response";
+import { Link, useParams } from "react-router-dom";
 import React from "react";
 import TopBar from "../TopBar/TopBar";
 import "./Artboard.css";
@@ -9,17 +9,47 @@ interface ArtboardDisplayProps {
 }
 
 function ArtboardView(props: ArtboardDisplayProps) {
-    let { artboardName } = useParams<{ artboardName: string }>();
-    let currentArtboard: Artboard;
-    currentArtboard = props.data.version.document.artboards.entries.filter(
-        (artboard) => {
-            console.log("artboardname", artboard.name, artboardName);
-            return artboard.name === artboardName;
-        }
+    let { artboardName, docId } = useParams<{
+        artboardName: string;
+        docId: string;
+    }>();
+
+    const artboards = props.data.version.document.artboards.entries;
+    let currentArtboard = artboards.filter(
+        (artboard) => artboard.name === artboardName
     )[0];
+    const currentArtboardNumber = artboards.indexOf(currentArtboard);
+    const allArtboardsNumber = artboards.length;
+    const prevArtboardName =
+        currentArtboardNumber > 1
+            ? artboards[currentArtboardNumber - 1].name
+            : null;
+    const nextArtboardName =
+        currentArtboardNumber < allArtboardsNumber
+            ? artboards[currentArtboardNumber + 1].name
+            : null;
+
     return (
         <div className="artboard">
-            <TopBar middle={artboardName}></TopBar>
+            <TopBar
+                left={
+                    <span>
+                        <Link to={`/${docId}`}>x</Link>|
+                        {prevArtboardName && (
+                            <Link to={`/${docId}/${prevArtboardName}`}>
+                                prev
+                            </Link>
+                        )}
+                        {currentArtboardNumber}/{allArtboardsNumber}
+                        {nextArtboardName && (
+                            <Link to={`/${docId}/${nextArtboardName}`}>
+                                next
+                            </Link>
+                        )}
+                    </span>
+                }
+                middle={artboardName}
+            />
             {currentArtboard && (
                 <img
                     className="artboard__img"
