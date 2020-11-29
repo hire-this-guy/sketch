55022c6a0e4b61fb5e98439865c70fad62349a1a
Krzyszof Lityński <klitynski@gog.com>
2020-11-29 16:14:01 +0100

    Fade in images

---
+++ src/components/FadeInImg/FadeInImg.tsx
@@ -0,0 +1,20 @@
+import React, { ImgHTMLAttributes, SyntheticEvent } from "react";
+import "./FadeInImg.css";
+
+function FadeInImg(props: ImgHTMLAttributes<unknown>) {
+    const onLoaded = (event: SyntheticEvent<HTMLImageElement, Event>) => {
+        event.currentTarget.classList.add("is-loaded");
+    };
+
+    // alt should be provided by users of the component
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return (
+        <img
+            {...props}
+            className={`image ${props?.className}`}
+            onLoad={onLoaded}
+        />
+    );
+}
+
+export default FadeInImg;
--- src/components/Thumbnail/Thumbnail.tsx
+++ src/components/Thumbnail/Thumbnail.tsx
@@ -2,13 +2,15 @@ import { Artboard } from "../../types/response";
 import React from "react";
 import "./Thumbnail.css";
 import { Link, useRouteMatch } from "react-router-dom";
+import FadeInImg from "../FadeInImg/FadeInImg";
 
 function Thumbnail(props: { artboard: Artboard }) {
     let { url } = useRouteMatch();
+
     return (
         <Link to={`${url}/${props.artboard.name}`} className="thumbnail">
             <figure className="thumbnail__figure">
-                <img
+                <FadeInImg
                     srcSet={`${props.artboard.files[0].url} 1x, ${props.artboard.files[1].url} 2x`}
                     alt=""
                     className="thumbnail__img"
